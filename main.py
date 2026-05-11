3ca0bb1723348c012c70346a83ba20708e7d6810
Jharana-oli <olijharana2004@gmail.com>
2026-05-11 22:03:23 +0545

    webcam configuration

--- main.py
+++ main.py
@@ -204,11 +204,23 @@ def main():
             rtc_configuration={
                 "iceServers": [
                     {"urls": ["stun:stun.l.google.com:19302"]},
+                    {"urls": ["stun:stun1.l.google.com:19302"]},
+                    {"urls": ["stun:stun2.l.google.com:19302"]},
                     {
                         "urls": ["turn:openrelay.metered.ca:80"],
                         "username": "openrelayproject",
-                        "credential": "openrelayproject",
+                        "credential": "openrelayproject"
                     },
+                    {
+                        "urls": ["turn:openrelay.metered.ca:443"],
+                        "username": "openrelayproject",
+                        "credential": "openrelayproject"
+                    },
+                    {
+                        "urls": ["turn:openrelay.metered.ca:443?transport=tcp"],
+                        "username": "openrelayproject",
+                        "credential": "openrelayproject"
+                    }
                 ]
             },
             media_stream_constraints={
@@ -217,7 +229,6 @@ def main():
             },
             async_processing=True
         )
-
         sync_metrics_update(context)
         if st.session_state.get("workout_completed"):
               st.success("🎉 All sets complete! Great work! Click 'End Workout' in the sidebar when you're ready.")
